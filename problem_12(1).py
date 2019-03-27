e05c14eaa57f573accb0a24ec4b76968462b1d25
ansaws <junk.ansaws@gmail.com>
2019-03-27 17:27:50 -0500

    prob 12 and 15 sucess

--- problem_12(1).py
+++ problem_12(1).py
@@ -7,15 +7,15 @@ divisors = 1
 while True:
     for number in range(1,num+1):
         tri_num += number
-    if num%2 == 0:
-        for div in range(1,round(tri_num/2)):
+    if tri_num%2 == 0:
+        for div in range(1,round(tri_num**(1/2))):
             if tri_num % div == 0:
                 divisors +=1
-    if num%2 == 1:
-        for div in range(1,round(tri_num/2),2):
+    if tri_num%2 == 1:
+        for div in range(1,round(tri_num**(1/2)),2):
             if tri_num % div == 0:
                 divisors +=1
-    if divisors > 500:
+    if divisors > 250:
         break
     tri_num = 0
     divisors = 1
--- problem_15.py
+++ problem_15.py
@@ -1,5 +1,15 @@
-x = []
-y = []
-for num in range(1,21):
-    x.append(num)
-    y.append(num)
+import math
+def double_factorial(x):
+	total = 1
+	factors = []
+	for num in range(x, 0, -2):
+		factors.append(num)
+	for factor in factors:
+		total *= factor
+	return total
+z = double_factorial(39)
+y = (2**20)*z
+final = y/(math.factorial(20))
+print(final)
+
+		
