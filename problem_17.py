f815beccce92bface42647ea6bebad63620bcf11
ansaws <junk.ansaws@gmail.com>
2019-03-29 19:08:16 -0500

    prob 17 finish and 18 start

--- problem_17.py
+++ problem_17.py
@@ -1,7 +1,8 @@
-one = ['one','two', 'three','four','five','six','seven','eight','nine','']
-ten = ['twenty','thirty','fourty','fifty','sixty', 'seventy','eighty','ninety','']
+one = ['','one','two', 'three','four','five','six','seven','eight','nine']
+ten = ['','twenty','thirty','forty','fifty','sixty', 'seventy','eighty','ninety']
 hundred = 'hundred'
 teen = ['ten', 'eleven', 'twelve', 'thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+hundreds = ["one",'two', 'three','four','five','six','seven','eight','nine']
 one_sum = 0
 teen_sum = 0
 ten_sum = 0
@@ -10,22 +11,37 @@ total_sum = 0
 #sum of teen's
 for item in teen:
     teen_sum += len(item)
-    hundred_sum += len(item) +len(hundred) +3
+    print(item)
 #sum of 2 digits and one's
-"""
-for num in ten:
-    for num_1 in one_1:
-        ten_sum +=len(num)+len(num_1)
-"""
-#sum of all but one's and teen's
-for num in one:
-    for num_1 in ten:
-        for num_2 in one:   
-            if len(num) == 0:
-                hundred_sum += -(len(hundred)+3) 
-            if len(num_1)==0 or len(num_2) == 0:
-                hundred_sum += -3
-            hundred_sum += len(num) + len(hundred) + 3 + len(num_1) + len(num_2) 
 
-total_sum = ten_sum+hundred_sum+teen_sum-6
+for num in ten:
+    for num_1 in one:
+        ten_sum +=len(num)+len(num_1)
+        print(num, num_1)
+#sum of hundreds
+for num in hundreds:
+    for item in teen:
+           hundred_sum += len(num) +len(hundred)+len("and")+len(item)
+           print(num, "hundred and", item)
+    for num_1 in ten:
+        for num_2 in one:
+            hundred_sum +=len(num)+ len(hundred)+len("and")+len(num_1)+len(num_2)
+            print(num , "hundred and", num_1, num_2)
+            if len(num_1)== 0 and len(num_2) ==0:
+                hundred_sum -= 3
+
+    
+    """
+    for num_1 in ten:
+        for num_2 in one:
+            hundred_sum += len(num_1)+len(num_2)
+        if len(num)==0 and len(num_1) == 0:
+            hundred_sum -= 3
+            """
+"""
+for num in hundreds:
+    for item in teen:
+        hundred_sum += len(num) +len(hundred)+len("and")+len(item)
+"""
+total_sum = ten_sum+hundred_sum+teen_sum+one_sum+len("onethousand")
 print(total_sum)
