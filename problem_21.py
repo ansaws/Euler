82177540a361321b4cc218a80d8fd721f1f806e6
ansaws <junk.ansaws@gmail.com>
2019-05-27 16:42:56 -0500

    finish problem 21

--- problem_21.py
+++ problem_21.py
@@ -1,33 +1,16 @@
-""""
-d = {}
-sum = 0
-for num in range(2,10000):
-
-    d[num] = sum
-    sum = 0
-sum = 0
-d[1] = 1
-
-for num in d:
-    x = d[num]
-    if d[x]==num:
-        sum+=num
-print(sum-1)
-"""
-
+#sucess
 def d(num):
     sum = 0
     for num_1 in range(1, num):
         if num%num_1 == 0:
             sum += num_1
     return sum
-
 sum = 0
 for a in range(1,10001):
     b = d(a)
-    if a == d(b):
+    if (a == d(b)) and (a != b):
         sum += a
-
+print(sum)
 
 
 
