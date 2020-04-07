e84208b5788543cb1b6f3bd0120d8f9261112d50
ansaws <junk.ansaws@gmail.com>
2020-04-07 17:32:17 -0500

    Update problem_26.py

--- problem_26.py
+++ problem_26.py
@@ -1,56 +1,38 @@
-"""
-from decimal import Decimal
-dec = None
-f = 0
-t = 0
-count = {}
-for num in range(11,1000,2):
-    dec =  list(str(Decimal(1/num)))
-    for de in range(2,len(dec)):
-        if num<100:
-            del dec[0:de]
-        else:
-            del dec[0:de]
-        f = dec[0]
-        for i in range(1,len(dec)):
-            dig = dec[i]
-            if dig==f:
-                break
-            else:
-                t+=1
-        count[t] = num
-        t = 0
-        dec =  list(str(Decimal(1/num)))
-print(count[max(count.keys())])
-"""
-f_dig= 0
-dig = 0
-rem = 1
+#RUNS INTO INFINITE LOOP OR JUST IS PLAIN INNEFICEINT AFTER MAX_COUNT = 19 OR SOMETIME AFTER NUM = 13
+n2= 10
+answer = []
+ans = 0
 count = 0
 max_count = 0
-rem2= 1
-max_num = 0 
-for num in range(9,1000,2):
-  while rem<num:
-    rem =rem* 10
-    count+=1
-  f_dig = rem//num
-  count +=1
-  rem = rem%num
-  rem2 = rem
-  while f_dig != dig:
-    while rem<num:
-      rem = rem *10
-      count +=1
-    dig = rem//num
-    count +=1
-    rem = rem%num
-    count+=1
-  if count>=max_count:
-    max_count = count
-    max_num= num
-    print(max_count, num)
+thing = 0
+for n1 in range(9,1001,2):
+  while n1 > n2:
+    n2 = n2*10   
+  while True:
+    while n1> n2:
+      n2 = n2*10
+      answer.append(0)
+    if n2%n1 == 0:
+      break
+      count =1
+    ans = n2//n1
+    n2 =n2 - (n1*ans)
+    for yeet in range(0, len(answer)-1):
+      if answer[yeet] == ans:
+        thing = yeet
+        break
+    if ans in answer:
+      del answer[:yeet]
+      break
+    else:
+      answer.append(ans)
+  if count ==1:
+    continue
   count = 0
-  rem2 = rem
-print(max_count, max_num)
-#RUNS INTO INFINITE LOOP OR JUST IS PLAIN INNEFICEINT AFTER MAX_COUNT = 19 OR SOMETIME AFTER NUM = 13
+  if len(answer)> max_count:
+    max_count = len(answer)
+  n2 = 10
+  answer = []
+print(max_count)
+
+
