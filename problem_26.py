fcb1c44721481949ecd68eec1387e7be823b0cff
ansaws <junk.ansaws@gmail.com>
2020-04-08 11:45:07 -0500

    updates

--- problem_26.py
+++ problem_26.py
@@ -13,8 +13,8 @@ for n1 in range(9,1001,2):
       n2 = n2*10
       answer.append(0)
     if n2%n1 == 0:
+      count = 1
       break
-      count =1
     ans = n2//n1
     n2 =n2 - (n1*ans)
     for yeet in range(0, len(answer)-1):
@@ -22,15 +22,15 @@ for n1 in range(9,1001,2):
         thing = yeet
         break
     if ans in answer:
-      del answer[:yeet]
+      del answer[:thing]
       break
     else:
       answer.append(ans)
   if count ==1:
     continue
-  count = 0
   if len(answer)> max_count:
     max_count = len(answer)
+  count = 0
   n2 = 10
   answer = []
 print(max_count)
--- problem_31.py
+++ problem_31.py
@@ -1,4 +1,4 @@
-t = 0
+"""t = 0
 onep = 0
 twop= 0
 fivep=0
@@ -25,15 +25,28 @@ for onep in range(0, 201):
             t+=1
             break
           elif onep+twop+fivep+tenp+twentp+oneE>200:
-            break
-          else:  
+            continue
+          else:
             for tenp in range(0,201,10):
-              for twentp in range(0,201,20):
-                for oneE in range(0, 201,100):
+              if onep+twop+fivep+tenp+twentp+oneE ==200:
+                t+=1
+                break
+              elif onep+twop+fivep+tenp+twentp+oneE >200:
+                break
+              else:
+                for twentp in range(0,201,20):
                   if onep+twop+fivep+tenp+twentp+oneE ==200:
                     t+=1
                     break
-                  if onep+twop+fivep+tenp+twentp+oneE>200:
+                  elif onep+twop+fivep+tenp+twentp+oneE >200:
                     break
-print(t+1)
+                  else:
+                    for oneE in range(0, 201,100):
+                      if onep+twop+fivep+tenp+twentp+oneE ==200:
+                        t+=1
+                        break
+                      if onep+twop+fivep+tenp+twentp+oneE>200:
+                        break
+print(t+1)"""
+
 
