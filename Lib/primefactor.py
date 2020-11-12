3f208a2c63ae98aa53ed93b075edd59cf714d760
ansaws <junk.ansaws@gmail.com>
2020-11-12 14:03:07 -0600

    solved 48 and added prime Factorization function

--- Lib/primefactor.py
+++ Lib/primefactor.py
@@ -1,22 +1,37 @@
-from lib.ISPRIME import isPrime
+def isPrime(num):
+  prime = True
+  if num%2 != 0:
+    for x in range(2,int(round(num**.5)+1)):
+      if num%x ==0:
+        prime = False
+        break
+  else:
+    prime = False
+  if num ==1:
+    prime = False
+  if num ==2:
+    prime = True
+  return prime
+
 def pFactor(num):
     final = {}
     nonPrime = num
     div = 2
     if isPrime(num) == True:
-        isPrime.append(num)
+        final[num] = 1
         return final
     while isPrime(nonPrime) == False:
-        while nonPrime%div !=0 and isPrime(div) == False:
+        while nonPrime%div !=0: #and isPrime(div) == False:
             div+=1
-        if(final.has_key(div)):
+        if div in final.keys():
             final[div] +=1
         else:
             final[div] = 1
         nonPrime /=div
         div = 2 
-     if(final.has_key(nonPrime)):
-            final[nonPrime] +=1
+    nonPrime = int(nonPrime)
+    if nonPrime in final.keys():
+        final[nonPrime] +=1
     else:
         final[nonPrime] = 1
     return final
--- problem_47.py
+++ problem_47.py
@@ -1,3 +1,37 @@
 from Lib.primefactor import pFactor
-print("hello world")
-print(pFactor(644))
+num = 10
+found = 0
+num_pFactor = {}
+check_pFactor = {}
+num_pair= []
+check_pair = []
+addFound = True
+while found<3:
+    found = 0
+    num +=1
+    num_pFactor = pFactor(num)
+    if(len(num_pFactor.keys()) ==4):
+        for check in range(num+1, num +4):
+            check_pFactor = pFactor(check)
+            if(len(check_pFactor.keys()) ==4):
+                for num_key in num_pFactor.keys():
+                    num_pair = [num_key, num_pFactor[num_key]]
+                    for check_key in check_pFactor.keys():
+                        check_pair = [check_key, check_pFactor[check_key]]
+                        if(check_pair == num_pair):
+                            addFound = False
+                if(addFound == False):
+                    break
+                else:
+                    found +=1
+            else:
+                break
+
+print(num)
+
+
+            
+        
+        
+
+
